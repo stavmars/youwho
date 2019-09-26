ed79a8b5bbc5d996d1d0f7fd383f1b568c4abc76
Stavros Maroulis <stavrosmrl@gmail.com>
2019-09-26 11:51:46 +0300

    Change question types

--- src/main/java/gr/ekke/youwho/domain/QuestionType.java
+++ src/main/java/gr/ekke/youwho/domain/QuestionType.java
@@ -8,12 +8,4 @@ public final class QuestionType {
     public static final String INFO_TEXT = "info_text";
     public static final String SINGLE_SELECT = "single_select";
     public static final String MULTI_SELECT = "multi_select";
-
-
-
-    public static final String LIKE_DISLIKE = "like_dislike";
-    public static final String SCALE_1_5 = "scale_1_5";
-    public static final String NUMERIC_SCALE_1_10 = "numeric_scale_1_10";
-    public static final String AGREEMENT_SCALE = "agreement_scale";
-    public static final String YES_NO = "yes_no";
 }
