89b4bb71320c74ef70e0cef8dac2f820cea3ed44
Stavros Maroulis <stavrosmrl@gmail.com>
2019-09-26 12:16:23 +0300

    Add description to response choices:wq

--- src/main/java/gr/ekke/youwho/domain/ResponseChoice.java
+++ src/main/java/gr/ekke/youwho/domain/ResponseChoice.java
@@ -16,6 +16,8 @@ public class ResponseChoice implements Serializable {
     @NotBlank
     private String text;
 
+    private String description;
+
     @NotNull
     private String type;
 
@@ -28,5 +30,59 @@ public class ResponseChoice implements Serializable {
     // if existing, it's the question to redirect the user to
     private String redirectQuestionId;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getResponseReaction() {
+        return responseReaction;
+    }
+
+    public void setResponseReaction(String responseReaction) {
+        this.responseReaction = responseReaction;
+    }
+
+    public String getRedirectQuestionId() {
+        return redirectQuestionId;
+    }
+
+    public void setRedirectQuestionId(String redirectQuestionId) {
+        this.redirectQuestionId = redirectQuestionId;
+    }
 }
