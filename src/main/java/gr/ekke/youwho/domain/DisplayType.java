852e614c85f34128435f691aaa8a9b6951a43570
Stavros Maroulis <stavrosmrl@gmail.com>
2019-09-26 12:03:33 +0300

    Modify question and display types

--- src/main/java/gr/ekke/youwho/domain/DisplayType.java
+++ src/main/java/gr/ekke/youwho/domain/DisplayType.java
@@ -4,8 +4,5 @@ package gr.ekke.youwho.domain;
  * Constants for Display Types.
  */
 public final class DisplayType {
-
-    public static final String SCALE = "scale";
     public static final String LIST = "list";
-
 }
--- src/main/java/gr/ekke/youwho/domain/QuestionType.java
+++ src/main/java/gr/ekke/youwho/domain/QuestionType.java
@@ -8,4 +8,6 @@ public final class QuestionType {
     public static final String INFO_TEXT = "info_text";
     public static final String SINGLE_SELECT = "single_select";
     public static final String MULTI_SELECT = "multi_select";
+    public static final String SCALE = "scale";
+
 }
