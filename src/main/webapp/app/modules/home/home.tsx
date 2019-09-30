d1fb6e88e729eb5ecf156f11429a44cc71481e70
Asteris <asteris.gian@gmail.com>
2019-09-30 18:15:23 +0300

    Moved react-simple-chatbot to home component.

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -2,8 +2,11 @@ import './home.scss';
 
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { ChatBot } from 'app/modules/chatbot/chatbot';
+// import YouWho from 'app/modules/chatbot/chatbot';
 import { IRootState } from 'app/shared/reducers';
+import ChatBot from 'react-simple-chatbot';
+import ThemeProvider from 'styled-components';
+import { configureStep } from 'app/modules/chatbot/configure-steps';
 import _ from 'lodash';
 
 export type IHomeProp = StateProps;
@@ -16,13 +19,48 @@ export class Home extends React.Component<IHomeProp> {
       return null;
     } else {
       const youWhoSurvey = surveysByName['youWho'];
+      const steps = configureStep(youWhoSurvey.questions);
 
       return (
-        <div>
-          <h1>{youWhoSurvey.name}</h1>
-          <p>{youWhoSurvey.description}</p>
-          <ChatBot survey={youWhoSurvey} />
-        </div>
+        <ChatBot
+          hideUserAvatar
+          botAvatar="/content/images/granny.png"
+          footerStyle={{ display: 'none' }}
+          headerComponent={
+            <div style={{ height: '70px', backgroundColor: '#FFFFFF' }}>
+              <img
+                src="/content/images/MobileLogo.svg"
+                alt="YouWho"
+                style={{
+                  top: 0,
+                  position: 'fixed',
+                  margin: '20px 0 0 20px'
+                }}
+              />
+              <img src="/content/images/dots.png" alt="Dots" style={{ float: 'right', margin: '25px 20px 0 0' }} />
+            </div>
+          }
+          steps={steps}
+          style={{
+            height: '100vh',
+            userSelect: 'none'
+          }}
+          contentStyle={{
+            height: 'calc(100% - 70px )',
+            width: window.innerWidth > 414 ? '380px' : '100%',
+            marginLeft: window.innerWidth > 414 ? '40%' : '0'
+          }}
+          bubbleStyle={{
+            userSelect: 'none'
+          }}
+          bubbleOptionStyle={{
+            background: 'rgba(255, 255, 255, 0.65)'
+          }}
+          customStyle={{
+            botBubbleColor: 'transparent'
+          }}
+          width="100%"
+        />
       );
     }
   }
