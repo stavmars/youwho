280fbef51b7c8254bdf6007d94130a146901ba3a
Asteris <asteris.gian@gmail.com>
2019-09-30 14:48:01 +0300

    Added imageURL on Question domain. Fixed typo on model.

--- src/main/webapp/app/shared/model/question.model.ts
+++ src/main/webapp/app/shared/model/question.model.ts
@@ -2,7 +2,7 @@ import { IResponseChoice } from 'app/shared/model/response-choice.model';
 
 export interface IQuestion {
   id?: string;
-  test?: string;
+  text?: string;
   type?: string;
   displayType?: string;
   category?: string;
