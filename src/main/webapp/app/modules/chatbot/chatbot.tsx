a083ff7481500cbfbed0b99bacdd34f00dd9c4e5
Asteris <asteris.gian@gmail.com>
2019-10-29 13:02:36 +0200

    Seperated chatbot from home componenent.

--- src/main/webapp/app/modules/chatbot/chatbot.tsx
+++ src/main/webapp/app/modules/chatbot/chatbot.tsx
@@ -1,71 +1,65 @@
-// import './chatbot.scss';
-//
-// import React from 'react';
-// import ChatBot from 'react-simple-chatbot';
-// import { ThemeProvider } from 'styled-components';
-// import { configureStep } from 'app/modules/chatbot/configure-steps';
-// // tslint:disable:jsx-no-lambda
-//
-// export interface IChatBotProps {
-//   survey: any;
-// }
-//
-// export const YouWho = (props: IChatBotProps) => {
-//   const { survey } = props;
-//   const steps = configureStep(survey.questions);
-//
-//   return (
-//     <ThemeProvider
-//       theme={{
-//         background: '#777EFF',
-//         fontFamily: 'TTNormsProMedium',
-//         botBubbleColor: '#FFFFFF',
-//         botFontColor: '#777EFF',
-//         userBubbleColor: 'rgba(255, 255, 255, 0.65)',
-//         userFontColor: '#777EFF'
-//       }}
-//     >
-//       <SimpleChatBot
-//         hideUserAvatar
-//         botAvatar="/content/images/granny.png"
-//         footerStyle={{ display: 'none' }}
-//         headerComponent={
-//           <div style={{ height: '70px', backgroundColor: '#FFFFFF' }}>
-//             <img
-//               src="/content/images/MobileLogo.svg"
-//               alt="YouWho"
-//               style={{
-//                 top: 0,
-//                 position: 'fixed',
-//                 margin: '20px 0 0 20px'
-//               }}
-//             />
-//             <img src="/content/images/dots.png" alt="Dots" style={{ float: 'right', margin: '25px 20px 0 0' }} />
-//           </div>
-//         }
-//         steps={steps}
-//         style={{
-//           height: '100vh',
-//           userSelect: 'none'
-//         }}
-//         contentStyle={{
-//           height: 'calc(100% - 70px )',
-//           width: window.innerWidth > 414 ? '380px' : '100%',
-//           marginLeft: window.innerWidth > 414 ? '40%' : '0'
-//         }}
-//         bubbleStyle={{
-//           userSelect: 'none'
-//         }}
-//         bubbleOptionStyle={{
-//           background: 'rgba(255, 255, 255, 0.65)'
-//         }}
-//         customStyle={{
-//           botBubbleColor: 'transparent'
-//         }}
-//         width="100%"
-//       />
-//     </ThemeProvider>
-//   );
-// };
-//
-// export default YouWho;
+import './chatbot.scss';
+
+import React from 'react';
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+import { configureStep } from 'app/modules/chatbot/configure-steps';
+import { ISurvey } from 'app/shared/model/survey.model';
+import moment, { Moment } from 'moment';
+// tslint:disable:jsx-no-lambda
+
+export interface IChatBotProps {
+  survey: ISurvey;
+  storeSurveyResponse(): void;
+  initiateSurveyResponse(survey: ISurvey, moment: Moment): void;
+}
+
+export const SurveyChat = (props: IChatBotProps) => {
+  const { survey } = props;
+  props.initiateSurveyResponse(survey, moment());
+  const steps = configureStep(survey.questions);
+
+  return (
+    <ThemeProvider
+      theme={{
+        background: '#777EFF',
+        fontFamily: 'TTNormsProMedium',
+        botBubbleColor: '#FFFFFF',
+        botFontColor: '#777EFF',
+        userBubbleColor: 'rgba(255, 255, 255, 0.65)',
+        userFontColor: '#777EFF'
+      }}
+    >
+      <ChatBot
+        hideUserAvatar
+        botAvatar="content/images/granny.png"
+        footerStyle={{ display: 'none' }}
+        hideHeader
+        handleEnd={() => props.storeSurveyResponse()}
+        steps={steps}
+        style={{
+          height: 'calc(100vh - 80px)',
+          userSelect: 'none',
+          borderRadius: '0px'
+        }}
+        contentStyle={{
+          height: window.innerWidth > 414 ? '70%' : '100%',
+          width: window.innerWidth > 414 ? '380px' : '100%',
+          marginLeft: window.innerWidth > 414 ? '40%' : '0'
+        }}
+        bubbleStyle={{
+          userSelect: 'none'
+        }}
+        bubbleOptionStyle={{
+          background: 'rgba(255, 255, 255, 0.65)'
+        }}
+        customStyle={{
+          botBubbleColor: 'transparent'
+        }}
+        width="100%"
+      />
+    </ThemeProvider>
+  );
+};
+
+export default SurveyChat;
--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,16 +1,11 @@
 import './home.scss';
-import './chatbot.scss';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-// import YouWho from 'app/modules/chatbot/chatbot';
 import { IRootState } from 'app/shared/reducers';
-import ChatBot from 'react-simple-chatbot';
-import { ThemeProvider } from 'styled-components';
-import { configureStep } from 'app/modules/chatbot/configure-steps';
+import SurveyChat from 'app/modules/chatbot/chatbot';
 import _ from 'lodash';
 import { initiateSurveyResponse, storeSurveyResponse } from 'app/modules/chatbot/chatbot.reducer';
-import moment from 'moment';
 // tslint:disable:jsx-no-lambda
 
 export interface IHomeProp extends StateProps, DispatchProps {}
@@ -23,49 +18,13 @@ export class Home extends React.Component<IHomeProp> {
       return null;
     } else {
       const youWhoSurvey = surveysByName['youWho'];
-      this.props.initiateSurveyResponse(youWhoSurvey, moment());
-      const steps = configureStep(youWhoSurvey.questions);
 
       return (
-        <ThemeProvider
-          theme={{
-            background: '#777EFF',
-            fontFamily: 'TTNormsProMedium',
-            botBubbleColor: '#FFFFFF',
-            botFontColor: '#777EFF',
-            userBubbleColor: 'rgba(255, 255, 255, 0.65)',
-            userFontColor: '#777EFF'
-          }}
-        >
-          <ChatBot
-            hideUserAvatar
-            botAvatar="content/images/granny.png"
-            footerStyle={{ display: 'none' }}
-            hideHeader
-            handleEnd={() => this.props.storeSurveyResponse()}
-            steps={steps}
-            style={{
-              height: 'calc(100vh - 80px)',
-              userSelect: 'none',
-              borderRadius: '0px'
-            }}
-            contentStyle={{
-              height: window.innerWidth > 414 ? '70%' : '100%',
-              width: window.innerWidth > 414 ? '380px' : '100%',
-              marginLeft: window.innerWidth > 414 ? '40%' : '0'
-            }}
-            bubbleStyle={{
-              userSelect: 'none'
-            }}
-            bubbleOptionStyle={{
-              background: 'rgba(255, 255, 255, 0.65)'
-            }}
-            customStyle={{
-              botBubbleColor: 'transparent'
-            }}
-            width="100%"
-          />
-        </ThemeProvider>
+        <SurveyChat
+          survey={youWhoSurvey}
+          initiateSurveyResponse={this.props.initiateSurveyResponse}
+          storeSurveyResponse={this.props.storeSurveyResponse}
+        />
       );
     }
   }
