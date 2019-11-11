f3984428725e2e83c4ebc191bc5f54eb58ec7c9b
Asteris <asteris.gian@gmail.com>
2019-11-11 12:14:10 +0200

    Fixed header for admin/user menu options.

--- src/main/webapp/app/shared/layout/menus/account.tsx
+++ src/main/webapp/app/shared/layout/menus/account.tsx
@@ -1,40 +1,15 @@
 import React from 'react';
-import MenuItem from 'app/shared/layout/menus/menu-item';
-import { DropdownItem } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavLink as Link } from 'react-router-dom';
+import { Dropdown } from 'semantic-ui-react';
 
-import { NavDropdown } from './menu-components';
-
-const accountMenuItemsAuthenticated = (
-  <>
-    <MenuItem icon="wrench" to="/account/settings">
-      Settings
-    </MenuItem>
-    <MenuItem icon="lock" to="/account/password">
-      Password
-    </MenuItem>
-    <MenuItem icon="sign-out-alt" to="/logout">
-      Sign out
-    </MenuItem>
-  </>
-);
-
-const accountMenuItems = (
-  <>
-    <MenuItem id="login-item" icon="sign-in-alt" to="/login">
-      Sign in
-    </MenuItem>
-    <MenuItem icon="sign-in-alt" to="/register">
-      Register
-    </MenuItem>
-  </>
-);
-
-export const AccountMenu = ({ isAuthenticated = false }) => (
-  <NavDropdown icon="user" name="Account" id="account-menu">
-    {isAuthenticated ? accountMenuItemsAuthenticated : accountMenuItems}
-  </NavDropdown>
+export const AccountMenu = () => (
+  <Dropdown item simple text={'Λογαριασμός'} id="account-menu">
+    <Dropdown.Menu>
+      <Dropdown.Item as={Link} to="/account/settings" icon="wrench" text={'Ρυθμίσεις'} />
+      <Dropdown.Item as={Link} to="/account/password" icon="clock" text={'Κωδικός'} />
+      <Dropdown.Item as={Link} to="/logout" icon="sign-out" text={'Αποσύνδεση'} />
+    </Dropdown.Menu>
+  </Dropdown>
 );
 
 export default AccountMenu;
