540c5db30e640b5872bc58a84b6f8192c4b91a9c
Asteris <asteris.gian@gmail.com>
2019-12-05 11:47:29 +0200

    Added results link mobile header.

--- src/main/webapp/app/shared/layout/header/header.tsx
+++ src/main/webapp/app/shared/layout/header/header.tsx
@@ -63,6 +63,18 @@ const Header = (props: IHeaderProps) => {
           <Menu.Item position="left" fitted style={{ marginLeft: '22px' }} as={Link} to="/">
             <Image src="content/images/MobileLogo.svg" />
           </Menu.Item>
+          <Menu.Item
+            style={{
+              font: 'Bold Italic 16px/34px TT Norms Pro',
+              letterSpacing: '0',
+              opacity: '1',
+              color: '#333333'
+            }}
+            as={Link}
+            to="/results/average"
+          >
+            Αποτελέσματα
+          </Menu.Item>
           <Menu.Item position="right" fitted onClick={props.toggleSidebar}>
             <Image src="content/images/dots.svg" style={{ width: '80px', height: '50px' }} />
           </Menu.Item>
