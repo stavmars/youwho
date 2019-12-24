62119318548a7051e9442c87f3edded13067c6e9
Asteris <asteris.gian@gmail.com>
2019-12-24 15:15:25 +0200

    Added YouTube video link. Only authorized. Removed video-react package.

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { IRootState } from 'app/shared/reducers';
 import { Button, Grid, Responsive, Image, Modal } from 'semantic-ui-react';
-import { Player, BigPlayButton } from 'video-react';
 
 // tslint:disable:jsx-no-lambda
 
@@ -27,6 +26,8 @@ export interface IHomeProp extends StateProps, DispatchProps {}
 
 export class Home extends React.Component<IHomeProp> {
   render() {
+    const { isAuthenticated } = this.props;
+
     return (
       <div className="home-container">
         <Responsive minWidth={Responsive.onlyTablet.minWidth}>
@@ -45,22 +46,28 @@ export class Home extends React.Component<IHomeProp> {
                   style={{ width: '20vw', height: '10vh', top: '2vh' }}
                 />
                 {subtext()}
-                <Modal
-                  trigger={
-                    <Button style={{ background: 'transparent', borderStyle: 'none', marginTop: '-20px' }}>
-                      <Image src="content/images/play-video.svg" inline style={{ width: '7vw', height: '7vh', marginRight: '2vw' }} />
-                      <span className="home-subtext">Δές το Βίντεο</span>
-                    </Button>
-                  }
-                  size="large"
-                >
-                  <Modal.Header>YouWho Promo Video</Modal.Header>
-                  <Modal.Content>
-                    <Player src="content/images/YouWho Promo-1.mp4">
-                      <BigPlayButton position="center" />
-                    </Player>
-                  </Modal.Content>
-                </Modal>
+                {isAuthenticated && (
+                  <Modal
+                    trigger={
+                      <Button style={{ background: 'transparent', borderStyle: 'none', marginTop: '-20px' }}>
+                        <Image src="content/images/play-video.svg" inline style={{ width: '7vw', height: '7vh', marginRight: '2vw' }} />
+                        <span className="home-subtext">Δές το Βίντεο</span>
+                      </Button>
+                    }
+                    basic
+                  >
+                    <Modal.Content>
+                      <iframe
+                        width="720"
+                        height="480"
+                        src="https://www.youtube.com/embed/AhveenOl5K8"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      />
+                    </Modal.Content>
+                  </Modal>
+                )}
               </Grid.Column>
             </Grid.Row>
           </Grid>
@@ -91,7 +98,9 @@ export class Home extends React.Component<IHomeProp> {
   }
 }
 
-const mapStateToProps = (storeState: IRootState) => ({});
+const mapStateToProps = (storeState: IRootState) => ({
+  isAuthenticated: storeState.authentication.isAuthenticated
+});
 
 const mapDispatchToProps = {};
 
