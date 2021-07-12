5e0722007e4e325c79e520218e52fa320019138b
Stavros Maroulis <stavrosmrl@gmail.com>
2021-07-12 10:34:23 +0300

    Change bubble text for students survey and make bubble text non hard coded

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { IRootState } from 'app/shared/reducers';
-import { Button, Grid, Responsive, Image, Modal } from 'semantic-ui-react';
+import { Button, Grid, Image, Modal, Responsive } from 'semantic-ui-react';
 import ReactPlayer from 'react-player';
 
 // tslint:disable:max-line-length
@@ -23,20 +23,46 @@ export class Home extends React.Component<IHomeProp> {
                 <Image src="content/images/YellowStripesBg.svg" className="stripes" />
               </Grid.Column>
               <Grid.Column width={7}>
-                <Image
-                  src={
-                    'content/images/Purple-YouWho-Εθνικό-Κέντρο-Κοινωνικών-Ερευνών-Γιουχου-National-Center-For-Social-Research-Erevna-Έρευνα-Νέων-Αποτελέσματα.png'
-                  }
-                  style={{ left: '-20vw', height: '40vh', zIndex: '1' }}
-                />
-                <Image
+                <div
+                  style={{
+                    position: 'relative',
+                    backgroundImage: 'url("content/images/Purple-bubble.png")',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    width: '60vh',
+                    marginLeft: '-20vw',
+                    height: '40vh',
+                    zIndex: 1
+                  }}
+                >
+                  <a href="/survey-chat/students" className="bubble-text">
+                    Είσαι φοιτητής/τρια; Πάρε μέρος στην νέα έρευνα YouWho? Students
+                  </a>
+                </div>
+                <div
+                  style={{
+                    position: 'relative',
+                    backgroundImage: 'url("content/images/Red-bubble.png")',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    width: '50vh',
+                    height: '30vh',
+                    marginLeft: '-5vw',
+                    zIndex: 1
+                  }}
+                >
+                  <a href="/menus/results" className="bubble-text">
+                    ... εξερευνήστε εδώ τα πιο πρόσφατα αποτελέσματα
+                  </a>
+                </div>
+                {/*<Image
                   src={
                     'content/images/Red-YouWho-Εθνικό-Κέντρο-Κοινωνικών-Ερευνών-Γιουχου-National-Center-For-Social-Research-Erevna-Έρευνα-Νέων-Αποτελέσματα.png'
                   }
                   as={Link}
                   to="/menus/results"
-                  style={{ left: '-5vw', width: '50vh' }}
-                />
+                  style={{left: '-5vw', width: '50vh'}}
+                />*/}
                 <div style={{ marginTop: '-45 vh' }}>
                   <Modal
                     trigger={
@@ -60,20 +86,53 @@ export class Home extends React.Component<IHomeProp> {
           <Grid centered>
             <Grid.Row style={{ padding: 0 }}>
               <Grid.Column width={14}>
-                <Image
+                <div
+                  style={{
+                    position: 'relative',
+                    backgroundImage: 'url("content/images/Purple-bubble.png")',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    width: '40vh',
+                    height: '30vh',
+                    marginLeft: '25vw',
+                    marginTop: '25vh',
+                    zIndex: 1
+                  }}
+                >
+                  <a href="/survey-chat/students" className="bubble-text">
+                    Είσαι φοιτητής/τρια; Πάρε μέρος στην νέα έρευνα YouWho? Students
+                  </a>
+                </div>
+                {/*                <Image
                   src={
                     'content/images/Purple-YouWho-Εθνικό-Κέντρο-Κοινωνικών-Ερευνών-Γιουχου-National-Center-For-Social-Research-Erevna-Έρευνα-Νέων-Αποτελέσματα-1.png'
                   }
-                  style={{ left: '15vw', top: '10vh', zIndex: '1' }}
-                />
-                <Image
+                  style={{left: '15vw', top: '10vh', zIndex: '1'}}
+                />*/}
+                <div
+                  style={{
+                    position: 'relative',
+                    backgroundImage: 'url("content/images/Red-bubble.png")',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    width: '35vh',
+                    height: '25vh',
+                    marginLeft: '35vw',
+                    zIndex: 1
+                  }}
+                >
+                  <a href="/menus/results" className="bubble-text">
+                    ... εξερευνήστε εδώ τα πιο πρόσφατα αποτελέσματα
+                  </a>
+                </div>
+                {/*<Image
                   src={
                     'content/images/Red-YouWho-Εθνικό-Κέντρο-Κοινωνικών-Ερευνών-Γιουχου-National-Center-For-Social-Research-Erevna-Έρευνα-Νέων-Αποτελέσματα-1.png'
                   }
                   as={Link}
                   to="/menus/results"
-                  style={{ height: '15vh', left: '20vw', top: '-12vh', float: 'right', zIndex: '1' }}
-                />
+                  style={{height: '15vh', left: '20vw', top: '-12vh', float: 'right', zIndex: '1'}}
+                />*/}
                 <Image src="content/images/giagia.png" className="granny" />
                 <Image src="content/images/YellowStripesBg.svg" alt="yellow-stripes" className="stripes" />
               </Grid.Column>
